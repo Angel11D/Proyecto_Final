8170ec6efc3bf2caa43c861b07a4045c9c70d9b7
Angel11D <gabolomarte@gmail.com>
2026-05-25 16:04:41 -0400

    Rename app to web in Flask application

--- web.py
+++ web.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+web = Flask(__name__)
 
-@app.route("/")
+@web.route("/")
 def inicio():
     return render_template("index.html")
 
-app.run(debug=True)
+web.run(debug=True)
