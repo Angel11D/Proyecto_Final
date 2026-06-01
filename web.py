4c854c248c0dba398df00ba2ef350e37e0b7ad8c
Angel11D <gabolomarte@gmail.com>
2026-06-01 18:11:40 -0400

    Update web.py

--- web.py
+++ web.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 web = Flask(__name__)
 
 @web.route("/")
 def inicio():
     return render_template("index.html")
+
+@web.route("/quiz", methods=["POST"])
+def quiz():
+
+    puntos = 0
+
+    if request.form["pregunta1"] == "b":
+        puntos += 1
+
+    if request.form["pregunta2"] == "a":
+        puntos += 1
+
+    if request.form["pregunta3"] == "a":
+        puntos += 1
+
+    return f"Obtuviste {puntos} de 3 puntos."
 
 web.run(debug=True)
